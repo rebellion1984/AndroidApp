0d82ae7971ed85da00438c497c18f497723f2c08
robelbeyene <robel.beyene@dsv.com>
2021-10-12 22:35:05 +0200

    show/hide image

--- app/src/main/java/com/robel/myfirstandroidapplication/MainActivity.java
+++ app/src/main/java/com/robel/myfirstandroidapplication/MainActivity.java
@@ -5,13 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private TextView txtCounter;
-    private Button btnPlus, btnMinus, btnReset;
+    private Button btnPlus, btnMinus, btnReset, btnHideshow;
+    private ImageView ivMeme;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +24,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnPlus = findViewById(R.id.btn_plus);
         btnMinus = findViewById(R.id.btn_minus);
         btnReset = findViewById(R.id.btn_reset);
+        ivMeme = findViewById(R.id.iv_meme);
+        btnHideshow=findViewById(R.id.btn_hide_show);
 
         btnPlus.setOnClickListener(this);
         btnMinus.setOnClickListener(this);
         btnReset.setOnClickListener(this);
+
+        btnHideshow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(ivMeme.getVisibility()== View.VISIBLE){
+                    ivMeme.setVisibility(View.GONE);
+                } else{
+                    ivMeme.setVisibility(View.VISIBLE);
+                }
+            }
+        });
     }
 
     @Override
