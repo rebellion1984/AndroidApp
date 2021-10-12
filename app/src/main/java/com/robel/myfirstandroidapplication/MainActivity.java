b294f2c4a90abce0b29bc93e8043ba799924731f
robelbeyene <robel.beyene@dsv.com>
2021-10-12 20:48:02 +0200

    Initial commit

---
+++ app/src/main/java/com/robel/myfirstandroidapplication/MainActivity.java
@@ -0,0 +1,99 @@
+package com.robel.myfirstandroidapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView txtCounter;
+    private Button btnPlus, btnMinus, btnReset;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txtCounter = findViewById(R.id.txt_counter);
+        btnPlus = findViewById(R.id.btn_plus);
+        btnMinus = findViewById(R.id.btn_minus);
+        btnReset = findViewById(R.id.btn_reset);
+        // solutioin #one
+
+        /*btnPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int counter = Integer.parseInt(txtCounter.getText().toString());
+                counter++;
+                txtCounter.setText(String.valueOf(counter));
+            }
+        });
+
+        btnMinus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int counter = Integer.parseInt(txtCounter.getText().toString());
+                if (counter != 0) {
+                    counter--;
+                    txtCounter.setText(String.valueOf(counter));
+                }
+            }
+        });
+
+        btnReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txtCounter.setText("0");
+            }
+        });*/
+
+    }
+
+    // solutioin #two
+    public void plusFunction(View view){
+        int counter = Integer.parseInt(txtCounter.getText().toString());
+        counter++;
+        txtCounter.setText(String.valueOf(counter));
+    }
+
+    public void minusFunction(View view) {
+        int counter = Integer.parseInt(txtCounter.getText().toString());
+        if (counter != 0) {
+            counter--;
+            txtCounter.setText(String.valueOf(counter));
+        }
+    }
+
+    public void resetFunction(View view) {
+        txtCounter.setText("0");
+    }
+
+    // solutioin #three
+    public void operation(View view) {
+        int id = view.getId();
+        int counter=0;
+        switch (id){
+            case R.id.btn_plus:
+                counter = Integer.parseInt(txtCounter.getText().toString());
+                counter++;
+                txtCounter.setText(String.valueOf(counter));
+                break;
+
+            case R.id.btn_minus:
+                counter = Integer.parseInt(txtCounter.getText().toString());
+                    if (counter != 0) {
+                        counter--;
+                        txtCounter.setText(String.valueOf(counter));
+                    }
+                break;
+
+            case R.id.btn_reset:
+                    txtCounter.setText("0");
+                break;
+
+        }
+    }
+}
